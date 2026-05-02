eff634fb95462bf7309409cf5fa4568090140629
Pedro <pedrxluismerinomunoz@gmail.com>
2026-05-02 15:00:59 +0200

    [SYC-9] Harden local service orchestration

--- cms/config/admin.js
+++ cms/config/admin.js
@@ -3,6 +3,13 @@
 module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
+    sessions: {
+      accessTokenLifespan: env.int('ADMIN_ACCESS_TOKEN_LIFESPAN', 30 * 60),
+      maxRefreshTokenLifespan: env.int('ADMIN_MAX_REFRESH_TOKEN_LIFESPAN', 30 * 24 * 60 * 60),
+      idleRefreshTokenLifespan: env.int('ADMIN_IDLE_REFRESH_TOKEN_LIFESPAN', 7 * 24 * 60 * 60),
+      maxSessionLifespan: env.int('ADMIN_MAX_SESSION_LIFESPAN', 7 * 24 * 60 * 60),
+      idleSessionLifespan: env.int('ADMIN_IDLE_SESSION_LIFESPAN', 60 * 60),
+    },
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
